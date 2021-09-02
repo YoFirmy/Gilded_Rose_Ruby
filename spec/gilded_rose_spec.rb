9c3cfce0cf0bd7842f3a8c2337c4c87b6615e133
Matt Firmston <mattfirmston@gmail.com>
2021-09-02 18:01:06 +0100

    split first test into feature test and isolated unit tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 require 'gilded_rose'
-require 'item'
 
 describe GildedRose do
+  let(:item) { double :item, name: 'foo', quality: 0, sell_in: 0, :sell_in= => -1 }
+  subject { described_class.new([item])}
+
   describe '#update_quality' do
     it 'does not change the name' do
-      items = [Item.new(name: 'foo', sell_in: 0, quality: 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].name).to eq 'foo'
+      subject.update_quality
+      expect(item).not_to receive(:name=)
     end
   end
 end
---
+++ spec/item_spec.rb
@@ -0,0 +1,14 @@
+require 'item'
+
+describe Item do
+  name = 'Example name'
+  sell_in = 10
+  quality = 5
+  subject { described_class.new(name, sell_in, quality) }
+
+  describe '#to_s' do
+    it 'displays the the attributes correctly' do
+      expect(subject.to_s).to eq("#{name}, #{sell_in}, #{quality}")
+    end
+  end
+end
