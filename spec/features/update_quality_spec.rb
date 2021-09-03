571a0eddd8eb5853ff0c9363f43929eeff3d1a11
Matt Firmston <mattfirmston@gmail.com>
2021-09-03 10:53:58 +0100

    created all tests for program

--- spec/features/update_quality_spec.rb
+++ spec/features/update_quality_spec.rb
@@ -3,12 +3,114 @@
 require 'gilded_rose'
 require 'item'
 
-describe GildedRose do
-  describe '#update_quality' do
+describe '#update_quality' do
+  context 'sellIn is more than 0' do
+    before(:each) do
+      @item = Item.new('foo', 5, 5)
+      GildedRose.new([@item]).update_quality
+    end
+
     it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].name).to eq 'foo'
+      expect(@item.name).to eq 'foo'
+    end
+
+    it 'reduces the sellIn by 1' do
+      expect(@item.sell_in).to eq(4)
+    end
+
+    it 'reduces the quality by 1' do
+      expect(@item.quality).to eq(4)
+    end
+
+    context 'item is Aged Brie' do
+      before(:each) do
+        @item = Item.new('Aged Brie', 5, 5)
+        GildedRose.new([@item]).update_quality
+      end
+  
+      it 'increases the quality' do
+        expect(@item.quality).to eq(6)
+      end
+    end
+  end
+
+  context 'sellIn is less than 0' do
+    before(:each) do
+      @item = Item.new('foo', -1, 5)
+      GildedRose.new([@item]).update_quality
+    end
+
+    it 'reduces the quality by 2' do
+      expect(@item.quality).to eq(3)
+    end
+
+    context 'item is Aged Brie' do
+      before(:each) do
+        @item = Item.new('Aged Brie', -1, 5)
+        GildedRose.new([@item]).update_quality
+      end
+  
+      it 'increases the quality by 2' do
+        expect(@item.quality).to eq(7)
+      end
+    end
+  end
+
+  context 'quality has reached 0' do
+    before(:each) do
+      @item = Item.new('foo', 5, 0)
+      GildedRose.new([@item]).update_quality
+    end
+
+    it 'does not reduce the quality' do
+      expect(@item.quality).to eq(0)
+    end
+  end
+
+  it 'does not increase the quality once quality has reached 50' do
+    @item = Item.new('Aged Brie', -1, 50)
+    GildedRose.new([@item]).update_quality
+    expect(@item.quality).to eq(50)
+  end
+
+  context 'item is Sulfuras' do
+    before(:each) do
+      @item = Item.new('Sulfuras, Hand of Ragnaros', 5, 5)
+      GildedRose.new([@item]).update_quality
+    end
+
+    it 'does not change the sellIn' do
+      expect(@item.sell_in).to eq(5)
+    end
+
+    it 'does note change the quality' do
+      expect(@item.quality).to eq(5)
+    end
+  end
+
+  context 'item is Backstage Passes' do
+    it 'increases the quality' do
+      @item = Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 5)
+      GildedRose.new([@item]).update_quality
+      expect(@item.quality).to eq(6)
+    end
+
+    it 'increases the quality by 2 when sellIn is 10 or less' do
+      @item = Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 5)
+      GildedRose.new([@item]).update_quality
+      expect(@item.quality).to eq(7)
+    end
+
+    it 'increases the quality by 3 when sellIn is 5 or less' do
+      @item = Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 5)
+      GildedRose.new([@item]).update_quality
+      expect(@item.quality).to eq(8)
+    end
+
+    it 'sets the quality to 0 when sellIn is less than 0' do
+      @item = Item.new('Backstage passes to a TAFKAL80ETC concert', -1, 5)
+      GildedRose.new([@item]).update_quality
+      expect(@item.quality).to eq(0)
     end
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -12,8 +12,85 @@ describe GildedRose do
       subject.update_quality
     end
 
-    it 'reduces the sellIn number by 1' do
+    it 'reduces the sellIn by 1' do
       expect(item).to receive(:sell_in=).with(item.sell_in - 1)
+      subject.update_quality
+    end
+
+    it 'reduces the quality by 1' do
+      expect(item).to receive(:quality=).with(item.quality - 1)
+      subject.update_quality
+    end
+
+    it 'reduces the quality by 2 when sellIn has passed' do
+      allow(item).to receive(:sell_in).and_return(-1)
+      expect(item).to receive(:quality=).with(item.quality - 1).twice
+      subject.update_quality
+    end
+
+    it 'does not reduce the quality once it has reached 0' do
+      allow(item).to receive(:quality).and_return(0)
+      expect(item).not_to receive(:quality=)
+      subject.update_quality
+    end
+
+    it 'increases the quality of Aged Brie' do
+      allow(item).to receive(:name).and_return('Aged Brie')
+      expect(item).to receive(:quality=).with(item.quality + 1)
+      subject.update_quality
+    end
+
+    it 'increases the quality of Aged Brie by 2 when sellIn has passed' do
+      allow(item).to receive(:sell_in).and_return(-1)
+      allow(item).to receive(:name).and_return('Aged Brie')
+      expect(item).to receive(:quality=).with(item.quality + 1).twice
+      subject.update_quality
+    end
+
+    it 'does not increase the quality once it has reached 50' do
+      allow(item).to receive(:quality).and_return(50)
+      allow(item).to receive(:name).and_return('Aged Brie')
+      expect(item).not_to receive(:quality=)
+      subject.update_quality
+    end
+
+    it 'does not change the sellIn of Sulfuras' do
+      allow(item).to receive(:name).and_return('Sulfuras, Hand of Ragnaros')
+      expect(item).not_to receive(:sell_in=)
+      subject.update_quality
+    end
+
+    it 'does note change the quality of Sulfuras' do
+      allow(item).to receive(:name).and_return('Sulfuras, Hand of Ragnaros')
+      expect(item).not_to receive(:quality=)
+      subject.update_quality
+    end
+
+    it 'increases the quality of Backstage Passes' do
+      allow(item).to receive(:name).and_return('Backstage passes to a TAFKAL80ETC concert')
+      allow(item).to receive(:sell_in).and_return(11)
+      expect(item).to receive(:quality=).with(item.quality + 1)
+      subject.update_quality
+    end
+
+    it 'increases the quality of Backstage Passes by 2 when sellIn is 10 or less' do
+      allow(item).to receive(:name).and_return('Backstage passes to a TAFKAL80ETC concert')
+      allow(item).to receive(:sell_in).and_return(10)
+      expect(item).to receive(:quality=).with(item.quality + 1).twice
+      subject.update_quality
+    end
+
+    it 'increases the quality of Backstage Passes by 3 when sellIn is 5 or less' do
+      allow(item).to receive(:name).and_return('Backstage passes to a TAFKAL80ETC concert')
+      allow(item).to receive(:sell_in).and_return(5)
+      expect(item).to receive(:quality=).with(item.quality + 1).exactly(3).times
+      subject.update_quality
+    end
+
+    it 'sets the quality of Backstage Passes to 0 when sellIn is less than 0' do
+      allow(item).to receive(:name).and_return('Backstage passes to a TAFKAL80ETC concert')
+      allow(item).to receive(:sell_in).and_return(-1)
+      expect(item).to receive(:quality=).with(0)
       subject.update_quality
     end
   end
