fa20709f226fedfa7dc4c7971b326b8b3a987ff6
Matt Firmston <mattfirmston@gmail.com>
2021-09-03 09:41:06 +0100

    create test to check sellIn amount decreases

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,13 +3,18 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  let(:item) { double :item, name: 'foo', quality: 0, sell_in: 0, :sell_in= => -1 }
+  let(:item) { double :item, name: 'foo', quality: 5, sell_in: 5, :sell_in= => 4, :quality= => 4 }
   subject { described_class.new([item])}
 
   describe '#update_quality' do
     it 'does not change the name' do
-      subject.update_quality
       expect(item).not_to receive(:name=)
+      subject.update_quality
+    end
+
+    it 'reduces the sellIn number by 1' do
+      expect(item).to receive(:sell_in=).with(item.sell_in - 1)
+      subject.update_quality
     end
   end
 end
