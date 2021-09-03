3c8754cd4a424954dfd221f8dae01d3f683b3d41
Matt Firmston <mattfirmston@gmail.com>
2021-09-03 13:00:27 +0100

    completed refactored code

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,25 +8,9 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if brie_or_tickets?(item)
-        increase_quality(item)
-        if backstage_passes?(item)
-          increase_quality(item) if item.sell_in < 11
-          increase_quality(item) if item.sell_in < 6
-        end
-      else
-        decrease_quality(item)
-      end
-      item.sell_in -= 1 unless sulfuras?(item)
-      if item.sell_in.negative?
-        if aged_brie?(item)
-          increase_quality(item)
-        elsif backstage_passes?(item)
-          item.quality = 0
-        else
-          decrease_quality(item)
-        end
-      end
+      edit_quality(item)
+      edit_sell_in(item)
+      edit_quality_when_sell_in_negative(item) if item.sell_in.negative?
     end
   end
 
@@ -54,5 +38,31 @@ class GildedRose
 
   def brie_or_tickets?(item)
     aged_brie?(item) || backstage_passes?(item)
+  end
+
+  def edit_quality(item)
+    if brie_or_tickets?(item)
+      backstage_passes?(item) ? increase_tickets(item) : increase_quality(item)
+    else
+      decrease_quality(item)
+    end
+  end
+
+  def increase_tickets(item)
+    increase_quality(item)
+    increase_quality(item) if item.sell_in < 11
+    increase_quality(item) if item.sell_in < 6
+  end
+
+  def edit_quality_when_sell_in_negative(item)
+    brie_or_tickets?(item) ? edit_negative_brie_or_tickets(item) : decrease_quality(item)
+  end
+
+  def edit_negative_brie_or_tickets(item)
+    aged_brie?(item) ? increase_quality(item) : item.quality = 0
+  end
+
+  def edit_sell_in(item)
+    item.sell_in -= 1 unless sulfuras?(item)
   end
 end
